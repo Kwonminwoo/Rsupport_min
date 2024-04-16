edd2360fcd7fadeb7bd37084d58e7d0eaac20a86
kwonMInwoo <qwerty029369@naver.com>
2024-04-16 14:44:09 +0900

    프로젝트 init

---
+++ src/main/java/rsupport/minwoo/notice_management/NoticeManagementApplication.java
@@ -0,0 +1,13 @@
+package rsupport.minwoo.notice_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rsupport/minwoo/notice_management/NoticeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package rsupport.minwoo.notice_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
