0595a77286ab20f485cbc6b1dd4024fbebe5ade6
kwonMInwoo <qwerty029369@naver.com>
2024-04-17 13:10:47 +0900

    feat: base response 반환 메서드 추가
    - base response 클래스에 반환 메서드를 추가

--- src/main/java/rsupport/minwoo/notice_management/global/base/ResponseAPI.java
+++ src/main/java/rsupport/minwoo/notice_management/global/base/ResponseAPI.java
@@ -1,22 +1,40 @@
 package rsupport.minwoo.notice_management.global.base;
 
-public class ResponseAPI<T> {
+import lombok.Builder;
+
+public abstract class ResponseAPI<T> {
     private T data;
     private String message;
     private int code;
 
-    public ResponseAPI(T data, String message, int code) {
+    @Builder
+    private ResponseAPI(T data, String message, int code) {
         this.data = data;
         this.message = message;
         this.code = code;
     }
 
-    public ResponseAPI(String message, int code) {
-        this.message = message;
-        this.code = code;
+    public static ResponseAPI<?> response(Object data, String message) {
+        return ResponseAPI.builder()
+            .data(data)
+            .message(message)
+            .build();
     }
 
-    public ResponseAPI(String message) {
-        this.message = message;
+    public static ResponseAPI<?> response(String message) {
+        return ResponseAPI.builder()
+            .message(message)
+            .build();
     }
+
+
+    public static ResponseAPI<?> response(Object data, String message, int code) {
+        return ResponseAPI.builder()
+            .data(data)
+            .message(message)
+            .code(code)
+            .build();
+    }
+
+
 }
