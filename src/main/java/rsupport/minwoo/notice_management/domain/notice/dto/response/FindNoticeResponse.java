e99f1fc0c912ed8cdcbc206bf57e0ed94d75ca9f
kwonMInwoo <qwerty029369@naver.com>
2024-04-22 22:43:56 +0900

    feat: 레디스 사용해 조회하도록 변경
    - 단건 조회 시 레디스 사용하도록 변경
    - 수정, 삭제 시 레디스 에서 삭제
    - 조회수 로직 레디스 활용하게 변경

--- src/main/java/rsupport/minwoo/notice_management/domain/notice/dto/response/FindNoticeResponse.java
+++ src/main/java/rsupport/minwoo/notice_management/domain/notice/dto/response/FindNoticeResponse.java
@@ -1,14 +1,24 @@
 package rsupport.minwoo.notice_management.domain.notice.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class FindNoticeResponse {
 
     private String title;
     private String content;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime postingDateTime;
     private long views;
     private String author;
